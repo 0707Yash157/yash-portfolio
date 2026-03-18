e611c62f696a3d7cf05bd4bf3df4a29f0099fe16
0707Yash157 <yashporwal494@gmail.com>
2026-03-18 20:52:18 +0530

    Enable dark mode and update content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  darkMode: "class", // IMPORTANT
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
